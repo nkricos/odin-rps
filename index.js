fe93a1b32e75ecbf0f086c81be625a58ee0edcff
nkricos <122754869+nkricos@users.noreply.github.com>
2023-05-17 15:46:38 -0400

    Create function getComputerChoice to generate rps

---
+++ index.js
@@ -0,0 +1,18 @@
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    let computerChoice;
+    switch(randomNumber) {
+        case 0: 
+            computerChoice = "Rock";
+            break;
+        case 1:
+            computerChoice = "Paper";
+            break;
+        case 2:
+            computerChoice = "Scissors";
+            break;
+    }
+    return computerChoice;
+}
+
+console.log(getComputerChoice());
